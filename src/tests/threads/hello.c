2f50a52644edc5cce55a8f420b4751dcc8b710db
zealotzealot <hsh0908y@gmail.com>
2017-09-12 22:04:11 +0900

    Upload Pintos files, with Project 0 Done

---
+++ src/tests/threads/hello.c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+#include "tests/threads/tests.h"
+#include "threads/init.h"
+#include "threads/malloc.h"
+#include "threads/synch.h"
+#include "threads/thread.h"
+#include "devices/timer.h"
+
+void test_hello (void) 
+{
+  printf("hello, world!\n");
+}
+
---
+++ src/threads/build/tests/threads/hello.d
@@ -0,0 +1,6 @@
+tests/threads/hello.o: ../../tests/threads/hello.c ../../lib/stdio.h \
+  ../../lib/debug.h ../../lib/stdarg.h ../../lib/stdbool.h \
+  ../../lib/stddef.h ../../lib/stdint.h ../../lib/kernel/stdio.h \
+  ../../tests/threads/tests.h ../../threads/init.h ../../threads/malloc.h \
+  ../../threads/synch.h ../../lib/kernel/list.h ../../threads/thread.h \
+  ../../devices/timer.h ../../lib/round.h
