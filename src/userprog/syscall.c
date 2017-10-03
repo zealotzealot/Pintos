d4a299787b98ae645ca3558895d6f72aac086f21
jaejun1 <ltq3021@gmail.com>
2017-10-03 21:57:20 +0900

    Argument Passing

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,8 +3,35 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler (struct intr_frame *);
+
+//Read a byte at user virtual address UADDR
+static int
+get_user (const uint8_t *uaddr)
+{
+  if(uaddr >= PHYS_BASE)
+    return -1;
+
+  int result;
+  asm ("movl $1f, %0; movzbl %1, %0; 1:"
+      : "=&a" (result) : "m" (*uaddr));
+  return result;
+}
+
+//Writes Byte to user address UDST
+static bool
+put_user(uint8_t *udst, uint8_t byte)
+{
+  if(udst >= PHYS_BASE)
+    return false;
+
+  int error_code;
+  asm ("movl $1f, %0; movb %b2, %1; 1:"
+      : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+  return error_code != -1;
+}
 
 void
 syscall_init (void) 
@@ -16,5 +43,7 @@ static void
 syscall_handler (struct intr_frame *f UNUSED) 
 {
   printf ("system call!\n");
+  printf ("%s\n",thread_current()->name);
+  hex_dump ( f->esp, f->esp, PHYS_BASE - f->esp, true);
   thread_exit ();
 }
