1cea734ee9e856d3908fd96eec39d648b65bf0ed
zealotzealot <hsh0908y@gmail.com>
2017-11-08 14:50:47 +0900

    Make stack growth boundary inclusive

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -156,7 +156,7 @@ page_fault (struct intr_frame *f)
   if (!user)
     exit(-1);
 
-  bool sg_1 = (fault_addr > f->esp) && (fault_addr < PHYS_BASE);
+  bool sg_1 = (fault_addr >= f->esp) && (fault_addr < PHYS_BASE);
   bool sg_2 = fault_addr == f->esp-4;
   bool sg_3 = fault_addr == f->esp-32;
   if (sg_1 || sg_2 || sg_3)
