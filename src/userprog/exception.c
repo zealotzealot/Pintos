1a13bca1c79034a9eee5c130df0bb638b4e8ed5f
zealotzealot <hsh0908y@gmail.com>
2017-11-12 04:22:24 +0900

    Fixed conditions for stack growth

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -165,9 +165,9 @@ page_fault (struct intr_frame *f)
     esp = thread_current()->esp;
 
   bool sg_1 = (fault_addr >= esp) && (fault_addr < PHYS_BASE);
-  bool sg_2 = fault_addr == esp-4;
-  bool sg_3 = fault_addr == esp-32;
-  bool sg_bad = (esp < PHYS_BASE - 0x80000);
+  bool sg_2 = fault_addr == f->esp-4;
+  bool sg_3 = fault_addr == f->esp-32;
+  bool sg_bad = (fault_addr < PHYS_BASE - 0x800000) || (fault_addr >= PHYS_BASE);
   if (!sg_bad && (sg_1 || sg_2 || sg_3))
     page_add_stack(fault_addr);
 
